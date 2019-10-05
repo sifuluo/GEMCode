b770f3eae61ae9c6bc02b9b96c63cb150e4e8393
Sven Dildick <sven.dildick@cern.ch>
2019-10-04 22:38:04 -0500

    Update scripts

--- GEMValidation/scripts/TriggerTDRStudies/plots_summary/Eff_ME1a_CLCT_Combined.C
+++ GEMValidation/scripts/TriggerTDRStudies/plots_summary/Eff_ME1a_CLCT_Combined.C
@@ -1,7 +1,7 @@
 void Eff_ME1a_CLCT_Combined()
 {
 //=========Macro generated from canvas: c/c
-//=========  (Fri Oct  4 22:30:06 2019) by ROOT version 6.14/09
+//=========  (Fri Oct  4 22:37:03 2019) by ROOT version 6.14/09
    TCanvas *c = new TCanvas("c", "c",0,0,800,600);
    gStyle->SetOptFit(1);
    gStyle->SetOptStat(0);
--- GEMValidation/scripts/TriggerTDRStudies/plots_summary/Eff_ME22_CLCT_Combined.C
+++ GEMValidation/scripts/TriggerTDRStudies/plots_summary/Eff_ME22_CLCT_Combined.C
@@ -1,7 +1,7 @@
 void Eff_ME22_CLCT_Combined()
 {
 //=========Macro generated from canvas: c/c
-//=========  (Fri Oct  4 22:30:14 2019) by ROOT version 6.14/09
+//=========  (Fri Oct  4 22:37:11 2019) by ROOT version 6.14/09
    TCanvas *c = new TCanvas("c", "c",0,0,800,600);
    gStyle->SetOptFit(1);
    gStyle->SetOptStat(0);
