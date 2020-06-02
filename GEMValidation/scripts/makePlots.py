f1568d05c4d5ab920516862def850d0205256d3a
Sven Dildick <sven.dildick@cern.ch>
2020-06-01 22:40:28 -0500

    put back in makeplots

--- GEMValidation/scripts/makePlots.py
+++ GEMValidation/scripts/makePlots.py
@@ -31,6 +31,13 @@ class GEMCSCStubPlotter():
 ## needs to be cleaned up
 plotter = GEMCSCStubPlotter()
 
+## plots - most do not work yet
+from efficiency.plots import *
+#from timing.plots import *
+#from occupancy.plots import *
+#from resolution.plots import *
+#from datavsemulator.plots import *
+
 CSCSimHit(plotter)
 CSCStripsWires(plotter)
 CSCStripsWires2(plotter)
--- GEMValidation/scripts/plots.py
+++
@@ -1,6 +0,0 @@
-## plots - most do not work yet
-from efficiency.plots import *
-#from timing.plots import *
-#from occupancy.plots import *
-#from resolution.plots import *
-#from datavsemulator.plots import *
